3ad4ffe0b15f5c9a65f1215ad34a6fac7485797c
Felipe Tadeu <souzalima@gmail.com>
2021-12-21 20:17:45 -0300

    day02

---
+++ python/day02/2-1.py
@@ -0,0 +1,18 @@
+def solve():
+	with open('input.txt') as infile:
+		data = [line.strip() for line in infile.readlines()]
+
+	horizontal = 0
+	vertical = 0
+	for i in range(0, len(data)):
+		if data[i].split()[0] == 'forward':
+			horizontal += int(data[i].split()[1])
+		elif data[i].split()[0] == 'up':
+			vertical -= int(data[i].split()[1])
+		elif data[i].split()[0] == 'down':
+			vertical += int(data[i].split()[1])
+	
+	print(horizontal * vertical)
+
+if __name__ == '__main__':
+	solve()
---
+++ python/day02/2-2.py
@@ -0,0 +1,22 @@
+def solve():
+	with open('input-test.txt') as infile:
+		data = [line.strip() for line in infile.readlines()]
+
+	horizontal = 0
+	vertical = 0
+	aim = 0
+	for i in range(0, len(data)):
+		if data[i].split()[0] == 'forward':
+			horizontal += int(data[i].split()[1])
+			vertical += aim * int(data[i].split()[1])
+		elif data[i].split()[0] == 'up':
+			vertical -= int(data[i].split()[1])
+			aim -= int(data[i].split()[1])
+		elif data[i].split()[0] == 'down':
+			vertical += int(data[i].split()[1])
+			aim += int(data[i].split()[1])
+	
+	print(horizontal * vertical)
+
+if __name__ == '__main__':
+	solve()
